b2c1df36c25c513d4bf6bdfe2cd86e3ff0c0c7f7
Personal <xinyan.lin@uwaterloo.ca>
2024-08-31 18:35:30 -0400

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'gradient-start': 'rgb(194, 255, 182)',
+        'gradient-middle-1': 'rgb(255, 163, 182)',
+        'gradient-middle-2': 'rgb(221, 169, 255)',
+        'gradient-end': 'rgb(162, 209, 255)',
+      },
       backgroundImage: {
+        "gradient-custom": "linear-gradient(to right, rgb(194, 255, 182), rgb(255, 163, 182), rgb(221, 169, 255), rgb(162, 209, 255))",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
